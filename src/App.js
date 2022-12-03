68a8988e19b98f1f3ea163a15ebf5031c5fdfac1
ramshadken <ram@user.com>
2022-12-03 14:32:09 +0530

    added calender component and added to app component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Calender from './Calender';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Calender></Calender>
       </header>
     </div>
   );
---
+++ src/Calender.js
@@ -0,0 +1,34 @@
+import Calendar from 'react-github-contribution-calendar';
+import React from 'react'
+
+function Calender() {
+    var values = {
+        '2016-06-21': 1,
+        '2016-06-22': 2,
+        '2016-06-25': 3,
+        '2016-06-27': 4,
+        '2016-06-29': 5,
+        '2016-06-30': 4,
+    }
+
+    var panelColors = [
+        '#EEEEEE',
+        'red',
+        'green',
+        'blue',
+        'yellow',
+        'cyan'
+
+    ];
+    var until = '2016-07-30';
+    return (
+        <>
+        <div>Calender</div>
+        <div>
+             <Calendar values={values} until={until} panelColors={panelColors} />
+        </div>
+        </>
+    )
+}
+
+export default Calender
